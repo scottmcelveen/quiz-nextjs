414030d82658944a5e1ae8a4d53c45e4ae04cb17
Scott McElveen <scott.mcelveen@hotmail.com>
2021-03-16 21:22:44 +0000

    add signinside as homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,8 @@
 import React from 'react';
-import Container from '@material-ui/core/Container';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-import ProTip from '../src/ProTip';
-import Link from '../src/Link';
-import Copyright from '../src/Copyright';
+import SignInSide from '../src/SignInSide';
 
-export default function Index() {
+export default function Signin() {
   return (
-    <Container maxWidth="sm">
-      <Box my={4}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Next.js with TypeScript example
-        </Typography>
-        <Link href="/about" color="secondary">
-          Go to the about page
-        </Link>
-        <ProTip />
-        <Copyright />
-      </Box>
-    </Container>
+    <SignInSide></SignInSide>
   );
 }
